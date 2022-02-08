0dd1c9397b7002c6319300ced2715551fd14c24d
maycon <maycon5591@gmail.com>
2022-02-08 16:13:22 -0300

    projeto finalizado

---
+++ src/main/java/com/mycompany/crud/Crud.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.crud;
+
+/**
+ *
+ * @author mayco
+ */
+public class Crud {
+    public static void main(String[] args) {
+        TesteConexao conexao = new TesteConexao();
+    } 
+}
---
+++ src/main/java/com/mycompany/crud/Pessoa.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.crud;
+
+/**
+ *
+ * @author thiag
+ */
+public class Pessoa {
+
+    private int id;
+    private String nome;
+    private byte[] imagem;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public byte[] getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(byte[] imagem) {
+        this.imagem = imagem;
+    }
+}
